dff1417e6981192ae7d96edb2861f5a2202aec07
k-kahora <mango22mjk@gmail.com>
2024-10-10 01:00:23 +0000

    more robust script

--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
+# Navigate to the directory where the script is located
 cd "$(dirname "$0")"
-just open
+
+# Check for the first argument passed to the script
+if [ "$1" == "toggle" ]; then
+    just toggle
+elif [ "$1" == "open" ]; then
+    just open
+else
+    echo "Usage: $0 {toggle|open}"
+    exit 1
+fi
