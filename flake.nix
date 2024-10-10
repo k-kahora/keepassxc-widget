67c8bbda99c48690f9d0e1a9ed1709b91b8c20cd
k-kahora <mango22mjk@gmail.com>
2024-10-10 00:44:58 +0000

    removed non needed inputs

--- flake.nix
+++ flake.nix
@@ -5,48 +5,31 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
-    quickshell = {
-      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    eww = { 
-      url = "github:elkowar/eww";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     ags = {
       url = "github:Aylur/ags";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, quickshell, ags, eww }:
+  outputs = { self, nixpkgs, flake-utils,  ags,  }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         lib = pkgs.lib;
         agsPkg = ags.packages.${system}."default";
-        ewwPkg = eww.packages.${system}."default";
-        quickshellPkg = quickshell.packages.${system}."default".override {
-          withHyprland = true;
-        };
       in
       {
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.keepassxc
-            pkgs.eww
-            pkgs.rofi-wayland
             pkgs.libsecret
             pkgs.adw-gtk3
             pkgs.nwg-look
-            quickshellPkg
             agsPkg
           ];
         };
 
-        packages.pass-launcher = pkgs.stdenv.mkDerivation {
+        packages.default = pkgs.stdenv.mkDerivation {
           name = "pass-launcher";
           nativeBuildInputs = [ pkgs.makeWrapper ];
           src = ./.;
